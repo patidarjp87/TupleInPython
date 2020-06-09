92f8409386bd59fdbef46a429ca34f02008ea089
jaydeep <jaydeeppatidar87@gmail.com>
2020-06-09 12:43:04 +0530

    update sorttuple file

--- sorttuple.py
+++ sorttuple.py
@@ -1,12 +1,18 @@
 print('program to sort tuple elements \n Enter a tuple')
 p=eval(input())
-i=0
-while i!=len(p)-1:
-    if p[i]>p[i+1]:
-        t=p[i]
-        p[i]=p[i+1]
-        p[i+1]=t
+l=list(p)
+length=len(l)
+r=1
+while r<length:
+    i=0
+    while i<length-r:
+        if l[i]>l[i+1]:
+            t=l[i]
+            l[i]=l[i+1]
+            l[i+1]=t
         i+=1
+    r+=1
 else:
+    p=tuple(l)
     print('sorted tuple is',p)
 input()
